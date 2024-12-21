c78cc4d2cb35c30fe8e9fb0b51ae934a7774a890
Bhola Chaudhary <hypr123@gmail.com>
2024-12-21 23:05:10 +0530

    done

--- Tasks/Task3/02.js
+++ Tasks/Task3/02.js
@@ -1,8 +1,6 @@
 
-// let arr = [1,5,3,1,3,5,6];
-let arr = [1,1,3,3,3,5,6,6];
-
-let copy = [...arr];
+let arr = [1,5,3,1,3,5,6];
+arr.sort((a, b) => a-b);
 
 for(let i=0;i<arr.length;i++){
     let count = 0;
